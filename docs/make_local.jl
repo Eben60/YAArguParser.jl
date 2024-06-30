95aca1af7ad3310aba871a8cf685064afcb737af
Ben Elkin <8xe8n-cm@online.de>
2024-06-30 21:33:16 +0200

    settong up CI

--- docs/make_local.jl
+++ docs/make_local.jl
@@ -1,9 +1,11 @@
-# using Pkg
-# Pkg.activate(@__DIR__)
-# Pkg.develop(path=(joinpath(@__DIR__, "../") |> normpath))
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(path=(joinpath(@__DIR__, "../") |> normpath))
 
 using Documenter, SimpleArgParse2
 
+# generate documentation locally. 
+# keep in mind .gitignore - deps/deps.jl
 makedocs(
     modules = [SimpleArgParse2],
     format = Documenter.HTML(; prettyurls = (get(ENV, "CI", nothing) == "true")),
@@ -17,13 +19,15 @@ makedocs(
         ],
     checkdocs = :exports, 
     warnonly = [:missing_docs],
+    # strict = true,
+    # clean = true,
 )
 ;
 
 # deployment done on the server anyway
 # don't normally run deploydocs here
-deploydocs(
-    repo = "github.com/Eben60/SimpleArgParse2.jl.git",
-    versions = nothing,
-    push_preview = true
-)
+# deploydocs(
+#     repo = "github.com/Eben60/SimpleArgParse2.jl.git",
+#     versions = nothing,
+#     push_preview = true
+# )
